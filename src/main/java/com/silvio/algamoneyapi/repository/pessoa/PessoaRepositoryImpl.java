b405fcbf0715512f8fbd5c4918881a6f762721f0
Silvio Ferreira Coutinho <silviofc79@gmail.com>
2018-09-17 14:01:21 -0300

    PessoaFilter - pesquisa por cidade (embedded)

--- src/main/java/com/silvio/algamoneyapi/repository/pessoa/PessoaRepositoryImpl.java
+++ src/main/java/com/silvio/algamoneyapi/repository/pessoa/PessoaRepositoryImpl.java
@@ -8,12 +8,14 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.springframework.util.StringUtils;
 
-import com.silvio.algamoneyapi.model.Lancamento;
+import com.silvio.algamoneyapi.model.Endereco;
+import com.silvio.algamoneyapi.model.Endereco_;
 import com.silvio.algamoneyapi.model.Pessoa;
 import com.silvio.algamoneyapi.model.Pessoa_;
 import com.silvio.algamoneyapi.repository.filter.PessoaFilter;
@@ -29,16 +31,19 @@ public class PessoaRepositoryImpl implements PessoaRepositoryQuery {
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
 		CriteriaQuery<Pessoa> criteria = builder.createQuery(Pessoa.class);
 		Root<Pessoa> root = criteria.from(Pessoa.class);
+		//Criacao de uma criteria para Join, nessa cada o campo endereco
+		//eh embedded attribute
+		Join<Pessoa, Endereco> criteriaJoin = root.join(Pessoa_.endereco);
 
 		// Criar restrincoes
-		Predicate[] predicates = criarRestricoes(filter, builder, root);
+		Predicate[] predicates = criarRestricoes(filter, builder, root, criteriaJoin);
 		criteria.where(predicates);
 
 		TypedQuery<Pessoa> query = manager.createQuery(criteria);
 		return query.getResultList();
 	}
 
-	private Predicate[] criarRestricoes(PessoaFilter filter, CriteriaBuilder builder, Root<Pessoa> root) {
+	private Predicate[] criarRestricoes(PessoaFilter filter, CriteriaBuilder builder, Root<Pessoa> root, Join<Pessoa, Endereco> criteriaJoin) {
 		
 		List<Predicate> predicates = new ArrayList<>();
 		
@@ -55,6 +60,14 @@ public class PessoaRepositoryImpl implements PessoaRepositoryQuery {
 		if (filter.getAtivo() != null && !filter.getAtivo() ) {
 			predicates.add(builder.isFalse(root.get(Pessoa_.ativo)));			
 		}
+		
+		//Teste para cidade
+		if (!StringUtils.isEmpty(filter.getCidade())) {
+			predicates.add(builder.like(
+					builder.lower(criteriaJoin.get(Endereco_.cidade)), "%" + filter.getCidade().toLowerCase() + "%"
+					));
+		}
+		//Fim teste para cidade
 
 		
 		return predicates.toArray(new Predicate[predicates.size()]);
