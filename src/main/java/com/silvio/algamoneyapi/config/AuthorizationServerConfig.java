b7c1eb950bf30ae1eb7a1ce138f7da05278d674c
Silvio Ferreira Coutinho <silviofc79@gmail.com>
2018-09-26 10:41:04 -0300

    Configurando o Refresh Token

--- src/main/java/com/silvio/algamoneyapi/config/AuthorizationServerConfig.java
+++ src/main/java/com/silvio/algamoneyapi/config/AuthorizationServerConfig.java
@@ -22,13 +22,15 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory().withClient("angular").secret("@angular0").scopes("read", "write")
-				.authorizedGrantTypes("password").accessTokenValiditySeconds(1800);
+				.authorizedGrantTypes("password", "refresh_token").accessTokenValiditySeconds(20)
+				.refreshTokenValiditySeconds(3600 * 24);
 
 	}
 
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.tokenStore(tokenStore()).accessTokenConverter(accessTokenConverter())
+				.reuseRefreshTokens(false)
 				.authenticationManager(authenticationManager);
 	}
 
