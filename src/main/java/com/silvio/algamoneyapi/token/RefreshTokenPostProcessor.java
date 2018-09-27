25a523d9cd8f3f673f1cb9cec71999f455107b1a
Silvio Ferreira Coutinho <silviofc79@gmail.com>
2018-09-27 15:35:07 -0300

    Finalizando o Token atraves de Cookies

--- src/main/java/com/silvio/algamoneyapi/token/RefreshTokenPostProcessor.java
+++ src/main/java/com/silvio/algamoneyapi/token/RefreshTokenPostProcessor.java
@@ -1,10 +1,17 @@
 package com.silvio.algamoneyapi.token;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
@@ -17,9 +24,32 @@ public class RefreshTokenPostProcessor implements ResponseBodyAdvice<OAuth2Acces
 			MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType,
 			ServerHttpRequest request, ServerHttpResponse response) {
 
-		String refreshToken = body.getRefreshToken().getValue();
+		HttpServletRequest req = ((ServletServerHttpRequest) request).getServletRequest();
+		HttpServletResponse resp = ((ServletServerHttpResponse) response).getServletResponse();
 
-		return null;
+		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) body;
+
+		String refreshToken = body.getRefreshToken().getValue();
+		adicionarRefreshTokenCookie(refreshToken, req, resp, 2592000);
+		removerRefreshTokenDoBody(token);
+
+		return body;
+	}
+
+	private void adicionarRefreshTokenCookie(String refreshToken, HttpServletRequest req, HttpServletResponse resp,
+			int expiry) {
+		Cookie refreshTokenCookie = new Cookie("refreshToken", refreshToken);
+		refreshTokenCookie.setHttpOnly(true);
+		refreshTokenCookie.setSecure(false); // mudar para true em producao, so funciona em HTTPS se for true
+		refreshTokenCookie.setPath(req.getContextPath() + "/oauth/token");
+		refreshTokenCookie.setMaxAge(expiry);
+		resp.addCookie(refreshTokenCookie);
+
+	}
+
+	private void removerRefreshTokenDoBody(DefaultOAuth2AccessToken token) {
+		token.setRefreshToken(null);
+
 	}
 
 	@Override
